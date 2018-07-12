6de9ece628c881d2e51dc05bda96495e8c6fa2eb
Sara Kim <sarakim0903@gmail.com>
2018-07-12 18:37:57 +0000

    automatically backed up by learn

---
+++ h.rb
@@ -0,0 +1,5 @@
+c = {
+  "tops" => ["tank", "t2"] ,
+  "bottoms" => ["short", "skirt"] }
+
+puts c["tops"][0]
--- lib/recycle.rb
+++ lib/recycle.rb
@@ -5,14 +5,14 @@
 # We haven't hard coded the hashes in the tests, 
 #they're dynamically brought in from the NYC Open Data API
 
-# first_recycle_bin =  {
-#   "address"=>"E 227 St/Bronx River Pkway",
-#   "site_type"=>"Subproperty",
-#   "longitude"=>"-73.864223918",
-#   "borough"=>"Bronx",
-#   "latitude"=>"40.890848989",
-#   "park_site_name"=>"227th St. Plgd"
-# }
+first_recycle_bin =  {
+  "address"=>"E 227 St/Bronx River Pkway",
+   "site_type"=>"Subproperty",
+   "longitude"=>"-73.864223918",
+   "borough"=>"Bronx",
+   "latitude"=>"40.890848989",
+  "park_site_name"=>"227th St. Plgd"
+ }
 
 
 #1. Define a method `address`, that takes a hash as an argument.
@@ -41,14 +41,14 @@ def borough(recycling_info)
 end
 
 def all_keys(recycling_info)
-  recycling_info.each_key do |info|
-    puts info
+  recycling_info.each do |key,value|
+    puts key
   end
 end
 
 def all_data(recycling_info)
-  recycling_info.each_value do |data|
-    puts data
+  recycling_info.each do |key,value|
+    puts value
   end
 end
 
